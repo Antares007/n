1ce1ccd8a9502a6f0e88ec2db3aa87f5e381646e
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-04-24 11:51:58 +0400

    parg

--- pith.c
+++ pith.c
@@ -1,15 +1,33 @@
 #define IMP
 #include "pith.h"
+////////////////////////////
+#define nargo(n) void n(void **o, void *begin, void *advance)
+#define arg(T, a) (*(T *)_advance = a, _advance += sizeof(T))
+#define parg(T, n) T n = *(T *)(advance = (char *)advance - sizeof(T))
+#define C(n, p, ...)                                                           \
+  {                                                                            \
+    char *_advance = (void *)advance;                                          \
+    __VA_ARGS__;                                                               \
+    ((void (*)(void *, void *, void *))n)(p, begin, (void *)_advance);         \
+  }
+#define Co(n, ...) C(o[n - 'a'], o, __VA_ARGS__)
+typedef void (*nt)(void **, void *, void *);
+
 #ifdef IMP
 ////////////////////////////
-#define pith(n, b, ...)                                                        \
-  void *n[32] = {__VA_ARGS__};                                                 \
-  for (int i = 0; i < 32; i++)                                                 \
-    if (n[i] == 0)                                                             \
-  n[i] = (void *)b[i]
+// 'a', 't', 'g', 'c', 0, 0, 0, o
+static nargo(r0) { (((nt *)o[7])[0])(o[7], begin, advance); }
+static nargo(r1) { (((nt *)o[7])[1])(o[7], begin, advance); }
+static nargo(r2) { (((nt *)o[7])[2])(o[7], begin, advance); }
+static nargo(r3) { (((nt *)o[7])[3])(o[7], begin, advance); }
+
+#define pith(n)                                                                \
+  void *n[32] = {                                                              \
+      ra, rb, rc, rd, re, rf, rg, rh, ri, rj, rk, rl, rm, rn, ro, rp,          \
+      rq, rr, rs, rt, ru, rv, rw, rx, ry, rz, 0,  0,  0,  0,  0,  n};
 #define ray(n, i)                                                              \
   static nargo(n) {                                                            \
-    ntype *on = (void *)(o[31]);                                               \
+    nt *on = (void *)o[31];                                                    \
     on[i]((void *)on, begin, advance);                                         \
   }
 ray(ra, 'a' - 'a') ray(rb, 'b' - 'a') ray(rc, 'c' - 'a') ray(rd, 'd' - 'a')
@@ -21,54 +39,71 @@ ray(ra, 'a' - 'a') ray(rb, 'b' - 'a') ray(rc, 'c' - 'a') ray(rd, 'd' - 'a')
                         ray(ru, 'u' - 'a') ray(rv, 'v' - 'a') ray(rw, 'w' - 'a')
                             ray(rx, 'x' - 'a') ray(ry, 'y' - 'a')
                                 ray(rz, 'z' - 'a');
-static void **trio() {
-  static void *p[32] = {
-      ra, rb, rc, rd, //
-      re, rf, rg, rh, //
-      ri, rj, rk, rl, //
-      rm, rn, ro, rp, //
-      rq, rr, rs, rt, //
-      ru, rv, rw,     //
-      rx, ry, rz,     //
-      0,  0,  0,  0,  //
-      0,  p           //
-  };
-  return p;
-}
 ////////////////////////////
+static nargo(mb26) { C(o[26], o[31]); }
 static nargo(mb27) { C(o[27], o[31]); }
 static nargo(mb28) { C(o[28], o[31]); }
 static nargo(mb29) { C(o[29], o[31]); }
 static nargo(mb30) { C(o[30], o[31]); }
 #include <stdint.h>
+// .... ....  .... ....
+// .... ....  .... ....
+// 0x1100
+// 0x0100
+// 0x1112
+// 0x0120
+// 0x1123
+//
+// C(mb, o, arg(nt, one), arg(nt, add2), 0)
+// C(mb, o, arg(nt, one), arg(nt, add2), 1) <--------
+// C(mb, o, arg(nt, one), arg(nt, add2), 1)
+// C(mb, o, arg(nt, one), arg(nt, add2), arg(nt, add2), 0x0200)
+// C(mb, o, arg(nt, one), arg(nt, add2), arg(nt, add2), arg(nt, add2), 0x1200)
+//
 static nargo(mb) {
-  param(uint64_t, rays);
-  pith(op, trio(), [31] = (void *)o);
-    
-  while (rays) {
-    int r = (rays & 0xff) - 'a';
-    param(ntype, n);
-    
-    rays = rays >> 8;
+  parg(uint64_t, cases);
+  parg(nt, alter);
+  pith(on);
+  on[31] = (void *)o;
+  if (cases) {
+    parg(nt, narb);
+    on[(cases & 0xff) - 'a'] = mb26, on[26] = narb;
+    if ((cases = cases >> 8)) {
+      parg(nt, narb);
+      on[(cases & 0xff) - 'a'] = mb27, on[27] = narb;
+      if ((cases = cases >> 8)) {
+        parg(nt, narb);
+        on[(cases & 0xff) - 'a'] = mb28, on[28] = narb;
+        if ((cases = cases >> 8)) {
+          parg(nt, narb);
+          on[(cases & 0xff) - 'a'] = mb29, on[29] = narb;
+        } else {
+        }
+      } else {
+      }
+    } else {
+    }
+  } else {
   }
-  param(ntype, nara);
-  C(nara, op);
+  parg(nt, nara);
+  C(nara, on);
 }
 static nargo(mbr) {
-  param(ntype, narb);
-  param(ntype, nara);
-  pith(op, trio(),                 //
-       ['r' - 'a'] = (void *)mb30, //
-       [30] = (void *)narb,        //
-       [31] = (void *)o            //
-  );
+  parg(nt, narb);
+  parg(nt, nara);
+  pith(op);
+  op['r' - 'a'] = (void *)mb30;
+  op[30] = (void *)narb;
+  op[31] = (void *)o;
   C(nara, op);
 }
 static nargo(mbl) {
-  param(ntype, narb);
-  param(ntype, nara);
-  pith(op, trio(),
-       ['l' - 'a'] = (void *)mb30, [31] = (void *)o, [30] = (void *)narb);
+  parg(nt, narb);
+  parg(nt, nara);
+  pith(op);
+  op['l' - 'a'] = (void *)mb30;
+  op[31] = (void *)o;
+  op[30] = (void *)narb;
   C(nara, op);
 }
 ////////////////////////////
@@ -100,7 +135,7 @@ void free(void *);
 #endif
 static nargo(one) { Co('r', arg(int, 1)); }
 static nargo(add2) {
-  param(int, a);
+  parg(int, a);
   Co('r', arg(int, a + 2));
 }
 static nargo(left) { Co('l'); }
@@ -111,11 +146,20 @@ static nargo(catch) {
   });
 }
 #include <stdint.h>
-int main() {
-  pith(p, trio(), ['l' - 'a'] = hexdump, ['r' - 'a'] = hexdump);
 
+nargo(nar1) {
+  uint64_t ui = 0x2736;
+  C(o[0], o, arg(int, ui >> 2 * 4 & 0xf));
+}
+
+int main() {
+  nt np[8] = {hexdump, hexdump};
+  pith(p);
+  p['l' - 'a'] = hexdump;
+  p['r' - 'a'] = hexdump;
   Ma(1 << 12, {
-    uint32_t i = 'abcd';
+    C(nar1, np);
+    uint32_t i = 'ijkl';
     uint64_t s = i;
     while (s) {
       C(hexdump, p, arg(int, s & 0xff));
@@ -123,8 +167,8 @@ int main() {
     }
   });
   // Ma(1 << 12, C(mbl, p,                            //
-  //               arg(ntype, one), arg(ntype, add2), //
-  //               arg(ntype, mbr), arg(ntype, add2), //
-  //               arg(ntype, mbr), arg(ntype, add2), //
-  //               arg(ntype, mbr), arg(ntype, catch)))
+  //               arg(nt, one), arg(nt, add2), //
+  //               arg(nt, mbr), arg(nt, add2), //
+  //               arg(nt, mbr), arg(nt, add2), //
+  //               arg(nt, mbr), arg(nt, catch)))
 }
--- pith.h
+++ pith.h
@@ -1,14 +1,1 @@
 #pragma once
-////////////////////////////
-#define nargo(n) void n(void **o, void *begin, void *advance)
-#define arg(T, a) (*(T *)_advance = a, _advance += sizeof(T))
-#define param(T, n) T n = *(T *)(advance = (char *)advance - sizeof(T))
-#define C(n, p, ...)                                                           \
-  {                                                                            \
-    char *_advance = (void *)advance;                                          \
-    __VA_ARGS__;                                                               \
-    ((void (*)(void *, void *, void *))n)(p, begin, (void *)_advance);         \
-  }
-#define Co(n, ...) C(o[n - 'a'], o, __VA_ARGS__)
-typedef void (*ntype)(void **, void *, void *);
-
