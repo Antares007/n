f3d350d4a0e3b5d3b4f2a9e508f93933723fc3c9
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-06-17 23:48:14 +0400

    mbo

--- g3.c
+++ g3.c
@@ -1,8 +1,8 @@
 typedef void (*nt)(void **, void *, void *);
 typedef void (*nargo_t)(nt *, void *, void *);
 #define nargo(n) void n(nt *ο, void *β, void *α)
-#define param(T, n) T n = *(T *)(α -= sizeof(void *))
-#define arg(T, a) (*(T *)οα = a, οα += sizeof(void *))
+#define param(T, n) T n = *(T *)(α -= sizeof(T))
+#define arg(T, a) (*(T *)οα = a, οα += sizeof(T))
 // colls
 #define C(n, p, ...)                                                           \
   {                                                                            \
@@ -11,10 +11,6 @@ typedef void (*nargo_t)(nt *, void *, void *);
     ((void (*)())n)(p, β, (void *)οα);                                         \
   }
 #define CR(ray, ...) C(ο[ray], ο, __VA_ARGS__)
-#define C0(...) CR(0, __VA_ARGS__)
-#define C1(...) CR(1, __VA_ARGS__)
-#define C2(...) CR(2, __VA_ARGS__)
-
 // void stars
 #define V(v) arg(void *, v)
 #define V2(v1, v2)                                                             \
@@ -23,13 +19,15 @@ typedef void (*nargo_t)(nt *, void *, void *);
 #define V3(v1, v2, v3)                                                         \
   V2(v1, v2);                                                                  \
   V(v3)
-
 // mbo
 #define Bn(ray, nexp)                                                          \
-  nb = οα;                                                                     \
-  nexp;                                                                        \
-  arg(void *, (void *)(οα - nb));                                              \
-  arg(void *, mb##ray)
+  {                                                                            \
+    void *nb = οα;                                                             \
+    nexp;                                                                      \
+    arg(int, (οα - nb));                                                       \
+  }                                                                            \
+  arg(int, ray);                                                               \
+  arg(void *, mbo)
 #define B2(ray, nexp1, nexp2)                                                  \
   nexp1;                                                                       \
   Bn(ray, nexp2)
@@ -41,21 +39,15 @@ typedef void (*nargo_t)(nt *, void *, void *);
   Bn(ray, nexp4)
 #define O(nexp)                                                                \
   {                                                                            \
-    void *οα = α, *nb;                                                         \
-    ((void)nb);                                                                \
+    void *οα = α;                                                              \
     nexp;                                                                      \
     οα -= sizeof(void *), (*(nargo_t *)οα)((void *)ο, β, οα);                  \
   }
-#define MBO(ray)                                                               \
-  nargo(mb##ray) {                                                             \
-    param(void *, size);                                                       \
-    printf("hey %p\n", size);                                                  \
-  }
 int printf(const char *, ...);
-MBO(0)
-MBO(1)
-MBO(2)
+nargo(mbo);
+nargo(r2) { CR(2); }
 nargo(r1) { CR(1); }
+nargo(r0) { CR(0); }
 nargo(lt) {
   param(int *, r);
   param(int *, l);
@@ -84,28 +76,71 @@ nargo(n12) {
   CR(1, V(&a));
 }
 typedef long int intptr_t;
-
-nargo(hexdump) {
-  for (; β < α; β++) {
-    unsigned char c = *(unsigned char *)β & 0xff;
-    if ((intptr_t)β % 16 == 0)
-      printf("\n%03lx: %02x", (intptr_t)β & 0xfff, c);
-    else if ((intptr_t)β % 8 == 0)
-      printf(" %02x", c);
-    else if ((intptr_t)β % 4 == 0)
-      printf(".%02x", c);
-    else
-      printf("%02x", c);
-  }
+typedef unsigned long int size_t;
+void *memcpy(void *destination, const void *source, size_t num);
+nargo(f0) {
+  nt *pith = (void *)ο[3];
+  C(pith[0], pith);
 }
+nargo(f1) {
+  nt *pith = (void *)ο[3];
+  C(pith[1], pith);
+}
+nargo(f2) {
+  nt *pith = (void *)ο[3];
+  C(pith[2], pith);
+}
+nargo(cb) {
+  nt *pith = (void *)ο[3];
+  intptr_t size = (intptr_t)ο[4];
+  memcpy(α, &ο[5], size);
+  α += size;
+  param(nargo_t, nar);
+  nar(pith, β, α);
+}
+nargo(mbo) {
+  param(int, ray);
+  param(int, size);
+  void *pith[5 + size / sizeof(void *)];
+  pith[0] = f0;
+  pith[1] = f1;
+  pith[2] = f2;
+  pith[ray] = cb;
+  pith[3] = ο;
+  pith[4] = (void *)(intptr_t)size;
+  memcpy(&pith[5], α = α - size, size);
+  param(nargo_t, nar);
+  nar((void *)pith, β, α);
+}
+
+#define Ray(n)                                                                 \
+  nargo(hexdump##n) {                                                          \
+    printf("\n%d", n);                                                         \
+    for (; β < α; β++) {                                                       \
+      unsigned char c = *(unsigned char *)β & 0xff;                            \
+      if ((intptr_t)β % 16 == 0)                                               \
+        printf("\n%03lx: %02x", (intptr_t)β & 0xfff, c);                       \
+      else if ((intptr_t)β % 8 == 0)                                           \
+        printf(" %02x", c);                                                    \
+      else if ((intptr_t)β % 4 == 0)                                           \
+        printf(".%02x", c);                                                    \
+      else                                                                     \
+        printf("%02x", c);                                                     \
+    }                                                                          \
+  }
+Ray(0);
+Ray(1);
+Ray(2);
 void *malloc(unsigned long);
 void free(void *);
-
+nargo(logint) {
+  param(int *, rez);
+  printf("%d\n", *rez);
+}
 int main() {
   void *β = malloc(1 << 12);
   void *α = β;
-  void *ο[] = {hexdump, hexdump, hexdump};
-
-  O(V3(n18, n12, gcd));
+  void *ο[] = {hexdump0, logint, hexdump2};
+  O(B3(1, V(n18), V(n12), V(gcd)));
   free(β);
 }
