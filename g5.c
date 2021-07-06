dd187edf7ff165973c65c30da95f25832cab827b
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-07-06 11:27:55 +0400

    ba_pith

--- g5.c
+++ g5.c
@@ -3,72 +3,72 @@
 #define N(n, T) void n(struct T *o, void *b, void *a)
 #define A(T, v) *(T *)a = v, a += sizeof(void *);
 #define P(T, n) T n = *(T *)(a -= sizeof(void *))
-struct ab_pith_s;
-typedef void (*ab_nar_t)(struct ab_pith_s *, void *, void *);
-struct ab_pith_s {
-  ab_nar_t a;
-  ab_nar_t b;
+struct ba_pith_s;
+typedef void (*ba_nar_t)(struct ba_pith_s *, void *, void *);
+struct ba_pith_s {
+  ba_nar_t b;
+  ba_nar_t a;
 };
-struct abo_pith_s;
-typedef void (*abo_nar_t)(struct abo_pith_s *, void *, void *);
-struct abo_pith_s {
-  struct ab_pith_s ab;
-  struct ab_pith_s *o;
-  ab_nar_t b;
+struct bao_pith_s;
+typedef void (*abo_nar_t)(struct bao_pith_s *, void *, void *);
+struct bao_pith_s {
+  struct ba_pith_s ba;
+  struct ba_pith_s *o;
+  ba_nar_t b;
 };
-N(mb_forward_b, abo_pith_s) { o->o->b(o->o, b, a); }
-N(mb_call_b, abo_pith_s) { o->b(o->o, b, a); }
-N(mb, ab_pith_s) {
-  P(ab_nar_t, narb);
-  P(ab_nar_t, nara);
-  struct abo_pith_s pith = {.ab =
+N(mb_forward_a, bao_pith_s) { o->o->a(o->o, b, a); }
+N(mb_call_b, bao_pith_s) { o->b(o->o, b, a); }
+N(mb, ba_pith_s) {
+  P(ba_nar_t, narb);
+  P(ba_nar_t, nara);
+  struct bao_pith_s pith = {.ba =
                                 {
-                                    .a = (ab_nar_t)mb_call_b,
-                                    .b = (ab_nar_t)mb_forward_b,
+                                    .a = (ba_nar_t)mb_forward_a,
+                                    .b = (ba_nar_t)mb_call_b,
                                 },
                             .o = o,
                             .b = narb};
-  nara((struct ab_pith_s *)&pith, b, a);
+  nara((struct ba_pith_s *)&pith, b, a);
 }
-N(one, ab_pith_s) {
+N(one, ba_pith_s) {
   A(int, 1);
   o->b(o, b, a);
 }
-N(add, ab_pith_s) {
+N(add, ba_pith_s) {
   P(int, r);
   P(int, l);
   A(int, l + r);
-  o->a(o, b, a);
+  o->b(o, b, a);
 }
-N(two, ab_pith_s) {
-  A(ab_nar_t, one)
-  A(ab_nar_t, one)
-  A(ab_nar_t, mb)
-  A(ab_nar_t, add)
+N(two, ba_pith_s) {
+  A(ba_nar_t, one)
+  A(ba_nar_t, one)
+  A(ba_nar_t, mb)
+  A(ba_nar_t, add)
   mb(o, b, a);
 }
-N(exam, ab_pith_s) {
-  A(ab_nar_t, one)
-  A(ab_nar_t, two)
-  A(ab_nar_t, mb)
-  A(ab_nar_t, two)
-  A(ab_nar_t, mb)
-  A(ab_nar_t, two)
-  A(ab_nar_t, mb)
-  A(ab_nar_t, add)
-  A(ab_nar_t, mb)
-  A(ab_nar_t, add)
-  A(ab_nar_t, mb)
-  A(ab_nar_t, add)
+N(exam, ba_pith_s) {
+  A(ba_nar_t, one)
+  A(ba_nar_t, two)
+  A(ba_nar_t, mb)
+  A(ba_nar_t, two)
+  A(ba_nar_t, mb)
+  A(ba_nar_t, two)
+  A(ba_nar_t, mb)
+  A(ba_nar_t, add)
+  A(ba_nar_t, mb)
+  A(ba_nar_t, add)
+  A(ba_nar_t, mb)
+  A(ba_nar_t, add)
   mb(o, b, a);
 }
-N(logint, ab_pith_s) {
+N(logint, ba_pith_s) {
   P(int, v);
   printf("%d\n", v);
 }
 int main() {
   void *b = malloc(1 << 12);
-  struct ab_pith_s o = {logint, logint};
+  struct ba_pith_s o = {logint, logint};
   exam(&o, b, b);
   free(b);
   return 0;
