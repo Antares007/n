67e1d505db53ebf5072e0460399ecc71fb1b8867
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-07-07 12:30:34 +0400

    ternary vs if then else

--- g5.c
+++ g5.c
@@ -43,6 +43,7 @@ N(ma, ba_pith_s) {
                             .b = narb};
   nara((struct ba_pith_s *)&pith, b, a);
 }
+
 N(one, ba_pith_s) {
   A(int, 1);
   o->b(o, b, a);
@@ -75,6 +76,7 @@ N(exam, ba_pith_s) {
   A(ba_nar_t, add);
   mb(o, b, a);
 }
+
 N(fibonacci_next, ba_pith_s) {
   P(int, c);
   P(int, p);
@@ -93,14 +95,33 @@ N(gcd, ba_pith_s) {
   : y < x ? (A(int, x - y), A(int, y), gcd(o, b, a))
           : (A(int, x), o->b(o, b, a));
 }
+N(gcd2, ba_pith_s) {
+  P(int, y);
+  P(int, x);
+  if (x < y)
+    (A(int, x), A(int, y - x), gcd(o, b, a));
+  else if (y < x)
+    (A(int, x - y), A(int, y), gcd(o, b, a));
+  else
+    (A(int, x), o->b(o, b, a));
+}
 int gcd_c(int x, int y) {
   return (x < y) ? gcd_c(x, y - x) : (y < x) ? gcd_c(x - y, y) : x;
+}
+int gcd2_c(int x, int y) {
+  if (x < y)
+    return gcd_c(x, y - x);
+  else if (y < x)
+    return gcd_c(x - y, y);
+  else
+    return x;
 }
 
 N(logint, ba_pith_s) {
   P(int, v);
   printf("%d\n", v);
 }
+
 int main() {
   void *b = malloc(1 << 12);
   struct ba_pith_s o = {logint, logint};
