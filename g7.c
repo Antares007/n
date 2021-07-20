c761dc0d437af2ae59ee3bfa7b526d6acbc5c95b
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-07-20 15:01:29 +0400

    renames

--- g7.c
+++ g7.c
@@ -6,20 +6,20 @@
 typedef void (*n_t)(void *, void *, void *);
 typedef uint64_t u64;
 #define Nba(n) void n(void *obr, void *abo, void *aba)
-#define Advance(T, v)                                                          \
-  assert(aba + sizeof(void *) <= obr), *(T *)aba = v, aba += sizeof(void *)
+#define Alloc(bts) assert(aba + bts <= obr), aba += bts
+#define Free(bts) assert(abo <= aba - bts), aba -= bts
+#define Olloc(bts) obr -= bts
+#define Oree(bts) obr += bts
+#define A(T, v) Aba(T, 0) = v, Alloc(sizeof(void *))
+// :: <T>(i) -> T
+#define Aba(T, i) (((T *)aba)[i])
 #define Obr(T, i) (((T *)obr)[i])
-#define ObrAdd(T, v)                                                           \
-  assert(aba <= obr - sizeof(T)), obr -= sizeof(T), *(T *)obr = v
+#define O(T, v) Olloc(sizeof(T)), Obr(T, 0) = v
 #define T obr, abo, aba
-#define O                                                                      \
-  do {                                                                         \
-    Retreat(n_t, nar);                                                         \
-    nar(obr, abo, aba);                                                        \
-  } while (0)
+#define C Free(sizeof(n_t)), Aba(n_t, 0)(obr, abo, aba);
 #define Retreat(T, n)                                                          \
-  assert(abo <= aba - sizeof(void *));                                         \
-  T n = *(T *)(aba -= sizeof(void *))
+  Free(sizeof(T));                                                             \
+  T n = Aba(T, 0)
 typedef int8_t b_t;
 typedef int16_t w_t;
 typedef int32_t d_t;
@@ -34,90 +34,90 @@ Nba(a) {}
 Nba(b) {}
 Nba(r0) { Obr(n_t, 0)(T); }
 Nba(r1) { Obr(n_t, 1)(T); }
-Nba(f0) { Obr(n_t, 3 + 0)(obr + sizeof(void *) * 3, abo, aba); }
-Nba(f1) { Obr(n_t, 3 + 1)(obr + sizeof(void *) * 3, abo, aba); }
-Nba(cb) {
-  n_t n = Obr(n_t, 2);
-  n(obr + sizeof(void *) * 3, abo, aba);
-}
+Nba(f0) { Oree(sizeof(void *) * 3), Obr(n_t, 0)(T); }
+Nba(f1) { Oree(sizeof(void *) * 3), Obr(n_t, 1)(T); }
+Nba(cb) { Oree(sizeof(void *) * 3), Obr(n_t, -1)(obr, abo, aba); }
 Nba(mb0) {
-  Retreat(n_t, narb);
-  ObrAdd(n_t, narb);
-  ObrAdd(n_t, f1);
-  ObrAdd(n_t, cb);
-  Retreat(n_t, nara);
+  Free(sizeof(void *));
+  n_t narb = Aba(n_t, 0);
+  Olloc(sizeof(n_t)), Obr(n_t, 0) = narb;
+  Olloc(sizeof(n_t)), Obr(n_t, 0) = f1;
+  Olloc(sizeof(n_t)), Obr(n_t, 0) = cb;
+  Free(sizeof(void *));
+  n_t nara = Aba(n_t, 0);
   nara(T);
 }
 Nba(mb1) {
   Retreat(n_t, narb);
-  ObrAdd(n_t, narb);
-  ObrAdd(n_t, cb);
-  ObrAdd(n_t, f0);
+  O(n_t, narb);
+  O(n_t, cb);
+  O(n_t, f0);
   Retreat(n_t, nara);
   nara(T);
 }
 // S = S ₁ a ₀ b.
 Nba(S) {
-  Advance(n_t, S);
-  Advance(n_t, a);
-  Advance(n_t, mb1);
-  Advance(n_t, b);
-  Advance(n_t, mb0);
-  O;
+  Alloc(sizeof(n_t) * 5);
+  Aba(n_t, 0) = S;
+  Aba(n_t, 1) = a;
+  Aba(n_t, 2) = mb1;
+  Aba(n_t, 3) = b;
+  Aba(n_t, 4) = mb0;
+  C;
 }
 Nba(S2) {
-  Advance(n_t, a);
-  Advance(n_t, S2);
-  Advance(n_t, mb1);
-  Advance(n_t, ε);
-  Advance(n_t, mb0);
-  O;
+  A(n_t, a);
+  A(n_t, S2);
+  A(n_t, mb1);
+  A(n_t, ε);
+  A(n_t, mb0);
+  C;
 }
 Nba(S1) {
-  Advance(n_t, b);
-  Advance(n_t, S2);
-  Advance(n_t, mb1);
-  O;
+  A(n_t, b);
+  A(n_t, S2);
+  A(n_t, mb1);
+  C;
 }
 Nba(one) {
-  Advance(int, 1);
+  A(int, 1);
   Obr(n_t, 1)(T);
 }
 Nba(add) {
   Retreat(int, r);
   Retreat(int, l);
-  Advance(int, l + r);
-  Advance(n_t, r1);
-  O;
+  A(int, l + r);
+  A(n_t, r1);
+  C;
 }
 Nba(two) {
-  Advance(n_t, one);
-  Advance(n_t, one);
-  Advance(n_t, mb1);
-  Advance(n_t, add);
-  Advance(n_t, mb1);
-  O;
+  A(n_t, one);
+  A(n_t, one);
+  A(n_t, mb1);
+  A(n_t, add);
+  A(n_t, mb1);
+  C;
 }
 Nba(exam) {
-  Advance(n_t, one);
-  Advance(n_t, two);
-  Advance(n_t, mb1);
-  Advance(n_t, two);
-  Advance(n_t, mb1);
-  Advance(n_t, two);
-  Advance(n_t, mb1);
-  Advance(n_t, add);
-  Advance(n_t, mb1);
-  Advance(n_t, add);
-  Advance(n_t, mb1);
-  Advance(n_t, add);
-  Advance(n_t, mb1);
-  O;
+  A(n_t, one);
+  A(n_t, two);
+  A(n_t, mb1);
+  A(n_t, two);
+  A(n_t, mb1);
+  A(n_t, two);
+  A(n_t, mb1);
+  A(n_t, add);
+  A(n_t, mb1);
+  A(n_t, add);
+  A(n_t, mb1);
+  A(n_t, add);
+  A(n_t, mb1);
+  C;
 }
 Nba(getdata) {
-  Advance(Q_t, 0x1122abbccddeeff0);
-  Advance(Q_t, 0x1122abbccddeeff0);
-  Advance(W_t, 0x9);
+  A(Q_t, 0x1122abbccddeeff0);
+  A(Q_t, 0x1122abbccddeeff0);
+  A(W_t, 0x9);
   Obr(n_t, 0)(T);
 }
 Nba(logint) {
@@ -149,11 +149,11 @@ int main() {
   void *obr, *abo, *aba;
   abo = aba = malloc(SS);
   obr = abo + SS;
-  ObrAdd(n_t, logint);
-  ObrAdd(n_t, dump);
-  Advance(n_t, exam);
-  O;
-  Advance(n_t, getdata);
-  O;
+  O(n_t, logint);
+  O(n_t, dump);
+  A(n_t, exam);
+  C;
+  A(n_t, getdata);
+  C;
   free(abo);
 }
