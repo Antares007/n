805714e584bd41164795f921f28251a25102eab2
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-07-21 17:55:36 +0400

    rbs

--- g7.c
+++ g7.c
@@ -1,41 +1,43 @@
-//           aba|              |obr
-//        abo - 0123456789abcdef - rbs
-//              XXXXXXXXXXXXXXXX
-//              rbp - fXXe
-//                    dXXc
-//                    bXXa
-//                    9XX8
-//                    7XX6
-//                    5XX4
-//                    3XX2
-//                    1XX0
-typedef void (*n_t)(void *, void *, void *);
-#define Nba(n) void n(void *obr, void *abo, void *aba)
-#define T obr, abo, aba
-#define A(T, v) Aba(T, 0) = (v), Alloc(sizeof(void *))
+//             init                           next
+// ...........................................................
+//    aba|              |obr    .             fXXe
+// abo - 0123456789abcdef - rbs .             dXXc
+//       XXXXXXXXXXXXXXXX       .        aba|     |obr
+//       rbp - fXXe             . abo - 0123456789abcdef - rbs
+//             dXXc             .       XXXXXXXXXXXXXXXX
+//             bXXa             .       rbp - bXXa
+//             9XX8             .             9XX8
+//             7XX6             .             7XX6
+//             5XX4             .             5XX4
+//             3XX2             .             3XX2
+//             1XX0             .             1XX0
+typedef void (*n_t)(void *, void *, void *, void *);
+#define Nba(n) void n(void *abo, void *aba, void *obr, void *rbs)
+#define T abo, aba, obr, rbs
+#define A(T, v) Aba(T, 0) = (v), Aalloc(sizeof(void *))
 #define Aba(T, i) (((T *)aba)[i])
-#define Alloc(bts) assert(aba + bts <= obr), aba += bts
-#define Free(bts) assert(abo <= aba - bts), aba -= bts
-#define C Free(sizeof(void *)), Aba(n_t, 0)(T)
+#define Aalloc(bts) assert(aba + bts <= obr), aba += bts
+#define Afree(bts) assert(abo <= aba - bts), aba -= bts
+#define C Afree(sizeof(void *)), Aba(n_t, 0)(T)
 #define R(T, n)                                                                \
-  Free(sizeof(void *));                                                        \
+  Afree(sizeof(void *));                                                       \
   T n = Aba(T, 0)
-#define O(T, v) Olloc(sizeof(T)), Obr(T, 0) = (v)
+#define O(T, v) Oalloc(sizeof(T)), Obr(T, 0) = (v)
 #define Obr(T, i) (((T *)obr)[i])
-#define Olloc(bts) assert(aba <= obr - bts), obr -= bts
-#define Oree(bts) obr += bts
+#define Oalloc(bts) assert(aba <= obr - bts), obr -= bts
+#define Ofree(bts) assert(obr + bts <= rbs), obr += bts
 #define Main(size, malloc, free, ...)                                          \
   int main() {                                                                 \
-    void *obr, *abo, *aba;                                                     \
-    abo = aba = malloc(size), obr = abo + size, __VA_ARGS__, free(abo);        \
+    void *abo, *aba, *obr, *rbs;                                               \
+    abo = aba = malloc(size), rbs = obr = abo + size, __VA_ARGS__, free(abo);  \
   }
 #include <assert.h>
 // f0 = rays:n_t[3] .[0].
-Nba(f0) { Oree(sizeof(void *) * 3), Obr(n_t, +0)(T); }
+Nba(f0) { Ofree(sizeof(void *) * 3), Obr(n_t, +0)(T); }
 // f1 = rays:n_t[3] .[1].
-Nba(f1) { Oree(sizeof(void *) * 3), Obr(n_t, +1)(T); }
+Nba(f1) { Ofree(sizeof(void *) * 3), Obr(n_t, +1)(T); }
 // cb = rays:n_t[3] rays[2].
-Nba(cb) { Oree(sizeof(void *) * 3), Obr(n_t, -1)(T); }
+Nba(cb) { Ofree(sizeof(void *) * 3), Obr(n_t, -1)(T); }
 // mbo = narb=n_t narb; f1; cb; nara=n_t nara.
 Nba(mbo) {
   R(n_t, narb);
