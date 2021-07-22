b503689cb4d2771860cb73f9d49c33f34f946e86
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-07-22 10:50:16 +0400

    show abi

--- abi.c
+++ abi.c
@@ -1,3 +1,6 @@
 void vs(int);
 int abi(long int a, long int b, int c, int d);
 void callsite() { vs(abi(-1, -2, -3, -4)); }
+int f(int x) { return x + 1; }
+int g(int x) { return x + 2; }
+int gof(int x) { return g(f(x)); }
