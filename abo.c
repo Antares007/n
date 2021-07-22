edb32da064d9b085cf4491eeaee5fec3c3ca107b
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-07-22 11:13:12 +0400

    add abo.c

---
+++ abo.c
@@ -0,0 +1,16 @@
+#include "m.h"
+Nba(mb0);
+Nba(f) {
+  R(int, x);
+  A(int, x + 1), Obr(n_t, 0)(T);
+}
+Nba(g) {
+  R(int, x);
+  A(int, x + 2), Obr(n_t, 0)(T);
+}
+Nba(gof) { A(n_t, f), A(n_t, g), mb0(T); }
+Nba(compose) {
+  R(n_t, f);
+  R(n_t, g);
+  A(n_t, f), A(n_t, g), mb0(T);
+}
