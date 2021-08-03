b0f684732fa0c4d6d25112f03f4aac55928da2c1
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-08-03 11:46:55 +0400

    fix talloc

--- gg.c
+++ gg.c
@@ -10,12 +10,16 @@ Nba(logint) {
   R(int, v);
   printf("A%d", v);
 }
+Nba(logerr) {
+  R(int, v);
+  printf("E%d", v);
+}
 Nba(talloc) {
   R(unsigned long, size);
   void *aba_ = aba + size;
   if (obr < aba_)
     return Obr(n_t, 2)(T);
-  for (int i = 1; i < ((unsigned long)(aba - abo) / sizeof(void *)) + 1; i++)
+  for (int i = 1; i < (unsigned long)(aba - abo) / sizeof(void *) + 1; i++)
     *(void **)(aba_ - i * sizeof(void *)) =
         *(void **)(aba - i * sizeof(void *));
   aba = aba_, A(void *, abo), abo += size, Obr(n_t, 1)(T);
@@ -23,9 +27,9 @@ Nba(talloc) {
 
 int main() {
   void *abo, *aba, *obr, *rbs;
-  abo = aba = malloc(10 << 12);
-  obr = rbs = aba + (10 << 12);
-  O(n_t, logint), O(n_t, logptr), O(n_t, logint);
-  A(unsigned long, 9), A(unsigned long, 16), talloc(T);
+  abo = aba = malloc(1 << 12);
+  obr = rbs = aba + (1 << 12);
+  O(n_t, logerr), O(n_t, logptr), O(n_t, logint);
+  A(unsigned long, 9), A(unsigned long, 2 << 10), talloc(T);
   free(abo);
 }
