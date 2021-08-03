13f138a4d9fc812003b9f198a6bfa0cc0a79574b
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-08-03 11:25:26 +0400

    talloc as word

--- gg.c
+++ gg.c
@@ -2,37 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+Nba(logptr) {
+  R(void *, v);
+  printf("ptr:%p", v);
+}
 Nba(logint) {
   R(int, v);
   printf("A%d", v);
 }
-Nba(logfloat) { printf("B"); }
-Nba(Hello) {
-  SubAba(sizeof(void *));
-  n_t nara = Aba(n_t, 0);
-  // R(n_t, nara);
-  Aba(n_t, 0)(T);
-  A(int, 9), Obr(n_t, 0)(T);
-
-  printf("%lu %d %d %lu\n", obr - aba, nara == logfloat, Obr(n_t, 0) == logint,
-         aba - abo);
-}
-Nba(ckaro) {
-  void *aba_ = aba + (16);
-  unsigned long n = ((unsigned long)(aba - abo)) / sizeof(void *);
-  int i = 0;
-  while (n--) {
-    i++;
+Nba(talloc) {
+  R(unsigned long, size);
+  void *aba_ = aba + size;
+  if (obr < aba_)
+    return Obr(n_t, 2)(T);
+  for (int i = 1; i < ((unsigned long)(aba - abo) / sizeof(void *)) + 1; i++)
     *(void **)(aba_ - i * sizeof(void *)) =
         *(void **)(aba - i * sizeof(void *));
-  }
-  aba = aba_;
+  aba = aba_, A(void *, abo), abo += size, Obr(n_t, 1)(T);
 }
 
 int main() {
   void *abo, *aba, *obr, *rbs;
   abo = aba = malloc(10 << 12);
   obr = rbs = aba + (10 << 12);
-  A(n_t, logfloat), O(n_t, logint), O(n_t, logint), O(n_t, logint), ckaro(T);
+  O(n_t, logint), O(n_t, logptr), O(n_t, logint);
+  A(unsigned long, 9), A(unsigned long, 16), talloc(T);
   free(abo);
 }
