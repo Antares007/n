21f374ab0fdff4f02692e6825456dbf2e6be0eee
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-08-09 19:42:09 +0400

    update fibonacci

--- src/afibonacci.c
+++ src/afibonacci.c
@@ -3,18 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 Sar(fibonacci_next, R(Q_t, c); R(Q_t, p); R(Q_t, n);
-                    A3(Q_t, n - 1, c, c + p) A(n_t, n ? fibonacci_next : O(0)))
+                    A3(Q_t, n - 1, c, c + p) A(n_t, n ? fibonacci_next : O(1)))
 S(rotate3);
 S(drop);
 S(swap);
-Nar(fibonacci,      A2(Q_t, 0, 1) A7(n_t, fibonacci_next, rotate3, an, drop, an, swap, an))
+Nar(fibonacci,      A2(Q_t, 0, 1) A7(n_t, fibonacci_next, rotate3, da, drop, da, swap, da))
 S(printfQQ);
 Main(               (1 << 12), malloc, free,
-                    A(n_t, printfQQ) P A(Q_t, 50) A(n_t, fibonacci) C)
+                    A2(n_t, printfQQ, printfQQ) PP A(Q_t, 50) A(n_t, fibonacci) C)
 Sar(rotate3,        R(Q_t, c); R(Q_t, b); R(Q_t, a);
-                    A3(Q_t, b, c, a) A(n_t, O(0)))
+                    A3(Q_t, b, c, a) A(n_t, O(1)))
 Sar(drop,           R(Q_t, a); ((void)a);
-                    A(n_t, O(0)))
+                    A(n_t, O(1)))
 Sar(swap,           R(void *, a); R(void *, b);
-                    A2(void*, a, b) A(n_t, O(0)))
+                    A2(void*, a, b) A(n_t, O(1)))
 S(printfQQ)         { R(Q_t, b); R(Q_t, a); printf("%lu %lu\n", a, b); }
