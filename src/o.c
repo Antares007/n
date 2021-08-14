367fd6dd50ba159c103fdc3d67381b319a74e4e6
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-08-14 14:11:24 +0400

    define N S

--- src/o.c
+++ src/o.c
@@ -13,13 +13,11 @@
                1XX0             .             1XX0
                         0 <= a <= r <= s                    */
 typedef void (*n_t)(void **o, long a, long r, long s);
-static void dddu(void **o, long a, long r, long s) {
-  r += 4, ((n_t *)o)[r - 1](o, a, r, s);
-}
-static void dddd0(void **o, long a, long r, long s) {
-  r += 4, ((n_t *)o)[r](o, a, r, s);
-}
-void da(void **o, long a, long r, long s) {
+#define N(n) void n(void **o, long a, long r, long s)
+#define S(n) static N(n)
+S(dddu) { r += 4, ((n_t *)o)[r - 1](o, a, r, s); }
+S(dddd0) { r += 4, ((n_t *)o)[r](o, a, r, s); }
+N(da) {
   n_t narb = o[--a];
   n_t nara = o[--a];
   n_t araarkhi = o[r + 2];
@@ -29,10 +27,8 @@ void da(void **o, long a, long r, long s) {
   o[--r] = dddd0;
   nara(o, a, r, s);
 }
-static void dddd1(void **o, long a, long r, long s) {
-  r += 4, ((n_t *)o)[r + 1](o, a, r, s);
-}
-void an(void **o, long a, long r, long s) {
+S(dddd1) { r += 4, ((n_t *)o)[r + 1](o, a, r, s); }
+N(an) {
   n_t narb = o[--a];
   n_t nara = o[--a];
   n_t araarkhi = o[r + 2];
@@ -42,43 +38,42 @@ void an(void **o, long a, long r, long s) {
   o[--r] = dddu;
   nara(o, a, r, s);
 }
-static void dot(void **o, long a, long r, long s) {
+S(dot) {
   n_t nar = o[--a];
   nar(o, a, r, s);
 }
-static void r1(void **o, long a, long r, long s) {
-  ((n_t *)o)[r + 1](o, a, r, s);
-}
-void one(void **o, long a, long r, long s) {
+S(r1) { ((n_t *)o)[r + 1](o, a, r, s); }
+N(one) {
   o[a++] = (void *)1;
   r1(o, a, r, s);
 }
-void add(void **o, long a, long r, long s) {
+N(add) {
   long rhs = (long)o[--a];
   long lhs = (long)o[--a];
   o[a++] = (void *)(rhs + lhs), r1(o, a, r, s);
 }
-void two(void **o, long a, long r, long s) {
+N(two) {
   o[a++] = one, o[a++] = one, o[a++] = da, o[a++] = add, o[a++] = da,
   dot(o, a, r, s);
 }
-void seven(void **o, long a, long r, long s) {
+N(seven) {
   o[a++] = one, o[a++] = two, o[a++] = da, o[a++] = add, o[a++] = da,
   o[a++] = two, o[a++] = da, o[a++] = add, o[a++] = da, o[a++] = two,
   o[a++] = da, o[a++] = add, o[a++] = da, dot(o, a, r, s);
 }
 #include <stdio.h>
 #include <stdlib.h>
-void aralog(void **o, long a, long b, long s) { printf("ara"); }
-void dalog(void **o, long a, long b, long s) {
+N(aralog) { printf("ara"); }
+N(dalog) {
   long v = (long)o[--a];
   printf("da %ld", v);
 }
-void anlog(void **o, long a, long b, long s) { printf("an"); }
+N(anlog) { printf("an"); }
 int main() {
   long a = 0, r, s = r = 512;
-  void **o = malloc(s * 8);
-  o[--r] = aralog, o[--r] = dalog, o[--r] = anlog, o[a++] = seven, dot(o, a, r, s);
+  void **o = malloc(s * sizeof(void *));
+  o[--r] = aralog, o[--r] = dalog, o[--r] = anlog, o[a++] = seven,
+  dot(o, a, r, s);
   free(o);
   return 0;
 }
