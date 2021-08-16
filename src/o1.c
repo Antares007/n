5fa82c3224facf031a93b791ea6d00a812519abb
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-08-16 15:08:11 +0400

    βαρσ

--- src/o1.c
+++ src/o1.c
@@ -1,76 +1,76 @@
-typedef void (*n_t)(void **ω, long α, long ρ, long σ);
-#define N(n) void(n)(void **ω, long α, long ρ, long σ)
+typedef void (*n_t)(void **β, long α, long ρ, long σ);
+#define N(n) void(n)(void **β, long α, long ρ, long σ)
 #define S(n) static N(n)
 #define Rc(n)                                                                  \
-  long n##σ = (long)ω[--α];                                                    \
-  long n##ρ = (long)ω[--α];                                                    \
-  long n##α = (long)ω[--α];                                                    \
-  void **n##ω = ω[--α]
+  long n##σ = (long)β[--α];                                                    \
+  long n##ρ = (long)β[--α];                                                    \
+  long n##α = (long)β[--α];                                                    \
+  void **n##β = β[--α]
 #define O(n, ...)                                                              \
   do {                                                                         \
-    void **_ω = n##ω;                                                          \
+    void **_ω = n##β;                                                          \
     long _α = n##α;                                                            \
     long _ρ = n##ρ;                                                            \
     long _σ = n##σ;                                                            \
-    void **ω = _ω;                                                             \
+    void **β = _ω;                                                             \
     long α = _α;                                                               \
     long ρ = _ρ;                                                               \
     long σ = _σ;                                                               \
-    __VA_ARGS__((n_t *)ω)[α - 1](ω, α - 1, ρ, σ);                              \
+    __VA_ARGS__((n_t *)β)[α - 1](β, α - 1, ρ, σ);                              \
   } while (0)
-S(dddu)  { O(, ρ += 3, ω[α++] = ω[ρ++], ); }
-S(dddd0) { O(, ρ += 4, ω[α++] = ω[ρ + 0], ); }
+S(dddu)  { O(, ρ += 3, β[α++] = β[ρ++], ); }
+S(dddd0) { O(, ρ += 4, β[α++] = β[ρ + 0], ); }
 N(da) {
-  n_t ararkhi = ω[ρ + 2];
-  O(, ω[--ρ] = ω[--α]
-    , ω[--ρ] = ararkhi
-    , ω[--ρ] = dddu
-    , ω[--ρ] = dddd0
+  n_t ararkhi = β[ρ + 2];
+  O(, β[--ρ] = β[--α]
+    , β[--ρ] = ararkhi
+    , β[--ρ] = dddu
+    , β[--ρ] = dddd0
     ,);
 }
-S(dddd1) { O(, ρ += 4, ω[α++] = ω[ρ + 1], ); }
+S(dddd1) { O(, ρ += 4, β[α++] = β[ρ + 1], ); }
 N(an) {
-  n_t ararkhi = ω[ρ + 2];
-  O(, ω[--ρ] = ω[--α]
-    , ω[--ρ] = ararkhi
-    , ω[--ρ] = dddd1
-    , ω[--ρ] = dddu
+  n_t ararkhi = β[ρ + 2];
+  O(, β[--ρ] = β[--α]
+    , β[--ρ] = ararkhi
+    , β[--ρ] = dddd1
+    , β[--ρ] = dddu
     ,);
 }
 N(one) {
-  O(, ω[α++] = (void*)1
-    , ω[α++] = ω[ρ + 1]
+  O(, β[α++] = (void*)1
+    , β[α++] = β[ρ + 1]
     ,);
 }
 N(add) {
-  long rhs = (long)ω[--α];
-  long lhs = (long)ω[--α];
-  O(, ω[α++] = (void *)(rhs + lhs)
-    , ω[α++] = ω[ρ + 1]
+  long rhs = (long)β[--α];
+  long lhs = (long)β[--α];
+  O(, β[α++] = (void *)(rhs + lhs)
+    , β[α++] = β[ρ + 1]
     ,);
 }
 N(two) { 
-  O(, ω[α++] = one
-    , ω[α++] = one
-    , ω[α++] = da
-    , ω[α++] = add
-    , ω[α++] = da 
+  O(, β[α++] = one
+    , β[α++] = one
+    , β[α++] = da
+    , β[α++] = add
+    , β[α++] = da 
     ,);
 }
 N(seven) {
-  O(, ω[α++] = one
-    , ω[α++] = two
-    , ω[α++] = da
-    , ω[α++] = add
-    , ω[α++] = da 
-    , ω[α++] = two
-    , ω[α++] = da
-    , ω[α++] = add
-    , ω[α++] = da 
-    , ω[α++] = two
-    , ω[α++] = da
-    , ω[α++] = add
-    , ω[α++] = da 
+  O(, β[α++] = one
+    , β[α++] = two
+    , β[α++] = da
+    , β[α++] = add
+    , β[α++] = da 
+    , β[α++] = two
+    , β[α++] = da
+    , β[α++] = add
+    , β[α++] = da 
+    , β[α++] = two
+    , β[α++] = da
+    , β[α++] = add
+    , β[α++] = da 
     ,);
 }
 
@@ -78,7 +78,7 @@ N(seven) {
 #include <stdlib.h>
 N(aralog) { printf("ara"); }
 N(dalog) {
-  void *v = ω[--α];
+  void *v = β[--α];
   printf("da %p", v);
 }
 N(anlog) { printf("an"); }
@@ -86,12 +86,12 @@ int main() {
   long α = 0;
   long ρ = 512;
   long σ = ρ;
-  void **ω = malloc(σ * sizeof(void *));
-  O(, ω[--ρ] = aralog
-    , ω[--ρ] = dalog
-    , ω[--ρ] = anlog
-    , ω[α++] = seven
+  void **β = malloc(σ * sizeof(void *));
+  O(, β[--ρ] = aralog
+    , β[--ρ] = dalog
+    , β[--ρ] = anlog
+    , β[α++] = seven
   ,);
-  free(ω);
+  free(β);
   return 0;
 }
