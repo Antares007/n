e7f7122fe1ecce12d1215c6177761561aa34dc00
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-09-24 18:26:28 +0400

    clean

--- c2.c
+++ c2.c
@@ -13,9 +13,6 @@ N(test) { C(, 1); }
 
 N(an_gadasvla) { C(, 0); }
 N(da_gadasvla) { C(, 1); }
-#define RAY                                                                    \
-  printf("%s\n", __FUNCTION__);                                                \
-  assert(σ[0].q == ρ)
 N(sss) {
   R(n_t, nar);
   R(p_t *, kalapoti);
@@ -23,12 +20,13 @@ N(sss) {
       shemdegi, da)
   O;
 }
-N(tskaro0) { RAY, A2(an_gadasvla, sss) O; }
-N(tskaro1) { RAY, A2(da_gadasvla, sss) O; }
-N(tskaro2) { RAY; }
-N(kalapoti0) { RAY, A2(da_gadasvla, sss) O; }
-N(kalapoti1) { RAY, A2(da_gadasvla, sss) O; }
-N(kalapoti2) { RAY; }
+#define PF printf("%s\n", __FUNCTION__)
+N(tskaro0) { PF,A2(an_gadasvla, sss) O; }
+N(tskaro1) { PF,A2(da_gadasvla, sss) O; }
+N(tskaro2) { PF;}
+N(kalapoti0) { PF,A2(da_gadasvla, sss) O; }
+N(kalapoti1) { PF,A2(da_gadasvla, sss) O; }
+N(kalapoti2) { PF;}
 N(tskaros_gulguli) {
   A9(tskaro0, tskaro1, tskaro2, 3, 6, 9, σ[-1].c, 7, akhali_gulguli) O;
 }
@@ -41,6 +39,7 @@ N(exam) {
   O;
 }
 N(tsinamkhare) {
+  // A9(exam, exam, da, exam, da, exam, da, exam, da) O;
   A7(exam, exam, da, exam, da, exam, da) O;
   // A5(exam, exam, da, exam, da) O;
   // A3(exam, exam, da) O;
@@ -52,32 +51,23 @@ N(tsinamkhare) {
 #define PS(p) (void *)((unsigned long)p & ~((unsigned long)0xFFF))
 #define PN(p) (PS(p) + 0x1000)
 N(os_akhali_purtseli) {
+  // printf("%s %ld\n", __FUNCTION__, (((unsigned long)σ) >> 12) - (((unsigned
+  // long)σ[1].v) >> 12));
   p_t *lx = σ[1].v;
   p_t *nο = PN(lx);
   nο += 3;
-  if (σ < nο) {
-    printf("ai\n");
+  if (σ < nο)
     return C(, 2);
-  }
   nο[-3].v = lx, nο[-2].v = σ, nο[-1].Q = 0;
   lx[-2].v = nο;
   σ[1].v = nο;
   A(nο) C(, 1);
 }
-
-//(p_t *) ο = 0x00007ffff7d76018
-//(long) α = 7
-//(long) ρ = -34
-//(p_t *) σ = 0x00007ffff7dd8fe8
-//(p_t *) lx = 0x00007ffff7d76018
-//(p_t *) begin = 0x00007ffff7d76000
-//(long) pages = 49
-//(unsigned long) wc = 7
 N(os_akhali_gulguli) {
   p_t *lx = σ[1].v;
   p_t *nσ = &lx[ISPAGE(lx) ? 506 : 512];
   long pages = (((unsigned long)σ >> 12) - ((unsigned long)nσ >> 12)) / 3;
-  printf("%ld\n",pages);
+  printf("%ld\n", pages);
   nσ = &nσ[pages * 512];
   long nρ = 0;
   R(unsigned long, wc);
@@ -102,13 +92,18 @@ typedef struct {
   p_t *ο;
   p_t *σ;
 } rigis_elementi_t;
-#define MAX_PID 1024
-rigis_elementi_t rigis_elementebi[MAX_PID];
-QUEUE queue;
-static int process_allocate_pid() {
+#define MAX_RIGIS_ELEMENTI 1024
+rigis_elementi_t rigis_elementebi[MAX_RIGIS_ELEMENTI];
+QUEUE rigis_tavi;
+static void init_rigi() {
+  for (int i = 0; i < MAX_RIGIS_ELEMENTI; i++)
+    rigis_elementebi[i].q[0] = 0;
+  QUEUE_INIT(&rigis_tavi);
+}
+static int shemdegi_rigis_nomeri() {
   static int last = 0;
   int i;
-  for (i = last + 1; i < MAX_PID; i++)
+  for (i = last + 1; i < MAX_RIGIS_ELEMENTI; i++)
     if (rigis_elementebi[i].q[0] == 0)
       return last = i, i;
   for (i = 1; i < last; i++)
@@ -117,17 +112,16 @@ static int process_allocate_pid() {
   return 0;
 }
 N(os_tsertili) {
-  int pid = process_allocate_pid();
-  if (pid == 0)
+  int nomeri = shemdegi_rigis_nomeri();
+  if (nomeri == 0)
     return C(, 2);
   R(p_t *, nσ);
   R(p_t *, nο);
-  rigis_elementebi[pid].ο = nο;
-  rigis_elementebi[pid].σ = nσ;
-  QUEUE_INSERT_TAIL(&queue, &rigis_elementebi[pid].q);
+  rigis_elementebi[nomeri].ο = nο;
+  rigis_elementebi[nomeri].σ = nσ;
+  QUEUE_INSERT_TAIL(&rigis_tavi, &rigis_elementebi[nomeri].q);
   C(, 1);
 }
-
 N(os_shemdegi) {
   ο[-1].Q = α;
   if (α == 0 && ο[-3].v != 0) {
@@ -136,7 +130,7 @@ N(os_shemdegi) {
     r[ISPAGE(r) ? -3 : 1].v = l;
   }
   QUEUE *q;
-  if (&queue != (q = QUEUE_NEXT(&queue))) {
+  if (&rigis_tavi != (q = QUEUE_NEXT(&rigis_tavi))) {
     QUEUE_REMOVE(q);
     rigis_elementi_t *e = (void *)q;
     e->q[0] = 0;
@@ -145,20 +139,14 @@ N(os_shemdegi) {
     eο[eα - 1].c(eο, eα - 1, eρ, eσ);
   }
 }
-n_t opcodes[] = {os_akhali_purtseli, os_akhali_gulguli, os_amotsere,
+n_t opcode_map[] = {os_akhali_purtseli, os_akhali_gulguli, os_amotsere,
                  os_tsertili, os_shemdegi};
-N(os) {
+N(os_opcode) {
   R(unsigned long, opcode);
-  opcodes[opcode](T());
+  opcode_map[opcode](T());
 }
-N(o0) { RAY, os_shemdegi(T()); }
-N(o1) { RAY, os_shemdegi(T()); }
-N(o2) { RAY, os_shemdegi(T()); }
-
 int main() {
-  for (int i = 0; i < MAX_PID; i++)
-    rigis_elementebi[i].q[0] = 0;
-  QUEUE_INIT(&queue);
+  init_rigi();
   // |.b..........|............|.e..........|............
   void *b = malloc(100 << 12);
   void *e = b + (100 << 12);
@@ -174,10 +162,10 @@ int main() {
 
   ο[-3].Q = 0, ο[-2].v = σ, ο[-1].Q = 0;
 
-  σ[--ρ].c = os;
-  σ[--ρ].c = o2;
-  σ[--ρ].c = o1;
-  σ[--ρ].c = o0;
+  σ[--ρ].c = os_opcode;
+  σ[--ρ].c = os_shemdegi;
+  σ[--ρ].c = os_shemdegi;
+  σ[--ρ].c = os_shemdegi;
   σ[0].q = ρ, σ[1].v = ο, σ[2].v = 0;
 
   tsinamkhare(ο, α, ρ, σ);
